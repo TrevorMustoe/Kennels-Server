694e99e7f3a6fa6f4a82f5f376e6f5b23ea07be5
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-10-19 12:16:54 -0400

    Finished modual 3

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,4 @@
-from .animal_requests import get_all_animals
+from .animal_requests import get_all_animals, get_single_animal
+from .locations_requests import get_single_location, get_all_locations
+from .employees_request import get_all_emplyees, get_single_employee
+from .customer_requests import get_single_customer, get_all_customers
